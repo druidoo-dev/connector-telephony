2ccca9b44c78f21ecff0f307681a7684f5eaaf1f
Alexis de Lattre <alexis@via.ecp.fr>
2014-08-26 00:25:27 +0200

    Feature "Open Calling Partner" replaced by "Open Caller", with a completely new behavior As a consequence, the modules asterisk_click2dial_event and asterisk_click2dial_crm_claim have been dropped New wizard "Number not found" (inspired by the old "Open Calling Partner" with many enhancements), which is called by base_phone_popup and Open Caller Formatting of number for click2dial is now handled by the phonenumbers lib: as a consequence, 3 fields have been dropped from asterisk.server (national_prefix, international_prefix, country_prefix) New option 'dial_button_invisible' in the phone widget New sample script asterisk_click2dial/scripts/openerp_popup_timeout.sh Update translations and some module descriptions All the modules are now PEP8 compliant

--- base_phone/__openerp__.py
+++ base_phone/__openerp__.py
@@ -60,6 +60,11 @@ Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for
         'res_users_view.xml',
         'wizard/reformat_all_phonenumbers_view.xml',
         'web_phone.xml',
+        'wizard/number_not_found_view.xml',
+        ],
+    'js': [
+        'static/src/js/*.js',
+        'static/lib/js/*.js',
         ],
     'qweb': ['static/src/xml/*.xml'],
     'demo': ['base_phone_demo.xml'],
