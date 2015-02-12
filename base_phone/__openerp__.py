549e72c6d3d1d573395002b7f515446a0891e2d0
Alexis de Lattre <alexis@via.ecp.fr>
2015-02-12 15:59:53 +0100

    base_phone: Add YAML test

--- base_phone/__openerp__.py
+++ base_phone/__openerp__.py
@@ -81,6 +81,7 @@ for any help or question about this module.
         ],
     'qweb': ['static/src/xml/*.xml'],
     'demo': ['base_phone_demo.xml'],
+    'test': ['test/phonenum.yml'],
     'images': [],
     'installable': True,
 }
